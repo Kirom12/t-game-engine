468a286897fa4d1cc4ada8951ba9c1ff37c938df
Baptiste Rosillon <batros.web@gmail.com>
2017-04-18 19:36:33 +0200

    removed distant directory

--- assets/scripts/javascript/tools/log.js
+++
@@ -1,8 +0,0 @@
-function Print(_text)
-{
-	console.log('%c' + _text, "background:#222; color:#bada55");
-}
-
-function ThrowErr(_text) {
-	throw _text;
-}
--- assets/scripts/javascript/tools/vector.js
+++
@@ -1,4 +0,0 @@
-function Vector(_x, _y) {
-	this.x = _x | 0;
-	this.y = _y | 0;
-}
